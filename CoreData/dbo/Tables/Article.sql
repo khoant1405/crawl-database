042fb1b5fb4012edd60d007c6bd4b245e06aab66
Khoa Nguyen Thanh <chelsea.edenhazard16@gmail.com>
2022-12-30 14:09:55 +0700

    update

--- CoreData/dbo/Tables/Article.sql
+++ CoreData/dbo/Tables/Article.sql
@@ -3,7 +3,7 @@
 	[ArticleName] [nvarchar](500) NOT NULL,
 	[Status] [nvarchar](255) NOT NULL,
 	[CreationDate] [datetime2](7) NOT NULL,
-	[CreationBy] [uniqueidentifier] NOT NULL,
+	[UserId] [uniqueidentifier] NOT NULL,
 	[RefURL] [nvarchar](255) NOT NULL,
 	[ImageThumb] [nvarchar](255) NULL,
 	[Description] [nvarchar](500) NULL,
@@ -13,4 +13,11 @@
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Article]  WITH CHECK ADD  CONSTRAINT [Fk_Article_UserId] FOREIGN KEY([UserId])
+REFERENCES [dbo].[User] ([Id])
+GO
+
+ALTER TABLE [dbo].[Article] CHECK CONSTRAINT [Fk_Article_UserId]
 GO
